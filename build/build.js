9fb3922492923b04d734813a72373dcc79d66d17
Leonardo Rafael Wehrmeister <leo.bnu@gmail.com>
2020-12-22 17:02:32 -0300

    More speed on build

--- build/build.js
+++ build/build.js
@@ -2,8 +2,9 @@ const clean = require("./clean.js");
 const Bundler = require("parcel-bundler");
 const minify = require('minify');
 const fs = require("fs-extra");
+const process = require('process');
 
-const bundler = new Bundler(["./src/app.js", "./src/cdn/*"], {
+const bundler = new Bundler("./src/app.js", {
   minify: false,
   watch: false,
   contentHash: false,
@@ -11,17 +12,10 @@ const bundler = new Bundler(["./src/app.js", "./src/cdn/*"], {
 });
 
 const onlyForCdn = async () => {
-  var files = {
-    './dist/cdn/beer.css': './dist/cdn/beer.min.css',
-    './dist/cdn/beer.js': './dist/cdn/beer.min.js'
-  }
-
-  await fs.copyFile('./src/cdn/beer.js', './dist/cdn/beer.js');
-
-  for (var i in files)
-    await minify(i).then((data) => fs.writeFile(files[i], data));
-
-  console.log("ready!");
+  await fs.mkdir('./dist/cdn');
+  process.chdir("./src/cdn");
+  minify('beer.js').then((data) => fs.writeFile('../../dist/cdn/beer.min.js', data));
+  minify('beer.css').then((data) => fs.writeFile('../../dist/cdn/beer.min.css', data));
 }
 
 bundler.on('bundled', onlyForCdn);
