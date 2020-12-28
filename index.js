a5ec0ebefc1b5774d8d3832b6223525abdd7d38d
Everton Alcides Buzzi <evertonbuzzi@gmail.com>
2020-12-28 10:15:30 -0300

    EB: import updates for NPM

---
+++ index.js
@@ -0,0 +1,2 @@
+import "beercss/dist/cdn/beer.min.css";
+import "beercss/dist/cdn/beer.min.js";                                                                                                                                                                 
