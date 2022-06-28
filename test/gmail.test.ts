64788de1c202e78c9856d83cca93992adb5eecc9
Leonardo Rafael Wehrmeister <>
2022-06-28 16:02:52 -0300

    Adding vitest to project

---
+++ test/gmail.test.ts
@@ -0,0 +1,50 @@
+import { expect, test, beforeAll } from "vitest";
+import domain from "../src/gmail/domain";
+import { IItemGmail } from "../src/gmail/interfaces";
+
+let emails:Array<IItemGmail>;
+
+beforeAll(() => {
+  emails = [
+    {
+      check: false,
+      star: false
+    },
+    {
+      check: false,
+      star: false
+    },
+    {
+      check: false,
+      star: false
+    }
+  ]
+});
+
+test("checkAll())", () => {
+  domain.checkAll(emails, true);
+  expect(emails.every((x) => x.check)).toBe(true);
+
+  domain.checkAll(emails, false);
+  expect(emails.every((x) => !x.check)).toBe(true);
+});
+
+test("check())", () => {
+  let firstEmail = emails[0];
+  
+  domain.check(firstEmail);
+  expect(firstEmail.check).toBe(true);
+
+  domain.check(firstEmail);
+  expect(firstEmail.check).toBe(false);
+});
+
+test("star())", () => {
+  let firstEmail = emails[0];
+  
+  domain.star(firstEmail);
+  expect(firstEmail.star).toBe(true);
+
+  domain.star(firstEmail);
+  expect(firstEmail.star).toBe(false);
+});
