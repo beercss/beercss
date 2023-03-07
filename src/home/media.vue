8018e6293041c303b7c7f63cd1f413d04d406c15
Leonardo Rafael Wehrmeister <leo.bnu@gmail.com>
2023-03-07 14:21:29 -0300

    Refactoring using :is operator

--- src/home/media.vue
+++ src/home/media.vue
@@ -4,7 +4,7 @@
   h4
     span Media
     a.chip.circle(
-      @click="domain.showSamples(data, '#media img, #media video', 'Media', null, 'https://github.com/beercss/beercss/blob/main/docs/MEDIA.md')"
+      @click="domain.showSamples(data, '#media img, #media video, #media svg', 'Media', null, 'https://github.com/beercss/beercss/blob/main/docs/MEDIA.md')"
     )
       i code
   nav.wrap.m.l
@@ -24,6 +24,14 @@
         name="media-images"
       )
       span videos
+    label.radio
+      input(
+        v-model="data.mediaImage",
+        :value="3",
+        type="radio",
+        name="media-images"
+      )
+      span svgs
   nav.wrap(v-if="data.mediaImage === 1")
     a
       img.circle.tiny(:src="'/beer-and-woman.jpg'")
@@ -118,6 +126,56 @@
         playsinline
       )
         source(:src="'/dance.mp4'", type="video/mp4")
+  nav.wrap(v-if="data.mediaImage === 3")
+    a
+      svg(viewBox="0 0 24 24").circle.tiny.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").circle.small.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").circle.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").circle.large.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").circle.extra.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").round.tiny.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").round.small.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").round.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").round.large.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").round.extra.primary
+        use(href="#account_circle")
+  nav.wrap(v-if="data.mediaImage === 3")
+    a
+      svg(viewBox="0 0 24 24").left-round.medium.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").top-round.medium.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").right-round.medium.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").bottom-round.medium.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").top-round.left-round.medium.primary
+        use(href="#account_circle")
+    a
+      svg(viewBox="0 0 24 24").bottom-round.right-round.medium.primary
+        use(href="#account_circle")
 </template>
 
 <script setup lang="ts">
