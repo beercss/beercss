7b2d5f338893e591395850ff907df6b1da605517
Leonardo Rafael Wehrmeister <>
2023-04-12 14:47:45 -0300

    Adding animation and helpers for tooltip element

--- src/home/page.vue
+++ src/home/page.vue
@@ -47,6 +47,10 @@ div
         i palette
       button.transparent.circle(data-ui="#more3")
         i collections
+      a.button.transparent.circle(href="https://github.com/beercss/beercss", target="_self")
+        i
+          svg(viewBox="0 0 24 24")
+            path(d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z")
 
   #modal-menu.modal.left
     header.fixed
@@ -54,7 +58,7 @@ div
         img.circle(:src="'/favicon.png'")
         .max
           h6 Beercss
-          div v3.1.0
+          div v3.1.1
         button.transparent.circle(data-ui="#modal-menu")
           i close
     a.row.round(@click="sharedDomain.updateMode(data)")
@@ -212,18 +216,15 @@ div
       .large-space
       .large-space
       nav.center-align
-        a.button.black-border.black-text.large.border(
-          href="https://www.npmjs.com/package/beercss",
-          target="_self"
-        ) NPM
-        a.button.black-border.black-text.large.border(
-          href="https://github.com/beercss/beercss",
-          target="_self"
-        ) Github
+        a.button.black-text.border.black-border.large(href="https://github.com/beercss/beercss/blob/main/docs/INDEX.md") Documentation
         a.button.black.white-text.large(
           href="https://github.com/beercss/beercss",
           target="_self"
-        ) Get started
+        )
+          i
+            svg(viewBox="0 0 24 24")
+              path(d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z")
+          span Get started
       .large-space
       .large-space
       .large-space
@@ -239,9 +240,9 @@ div
             .medium-space
             h5 From CDN
             pre.scroll.fill.no-round(style="font-family: courier new")
-              | &lt;link href="https://cdn.jsdelivr.net/npm/beercss@3.1.0/dist/cdn/beer.min.css" rel="stylesheet"&gt;
+              | &lt;link href="https://cdn.jsdelivr.net/npm/beercss@3.1.1/dist/cdn/beer.min.css" rel="stylesheet"&gt;
               |
-              | &lt;script type="module" src="https://cdn.jsdelivr.net/npm/beercss@3.1.0/dist/cdn/beer.min.js"&gt;&lt;/script&gt;
+              | &lt;script type="module" src="https://cdn.jsdelivr.net/npm/beercss@3.1.1/dist/cdn/beer.min.js"&gt;&lt;/script&gt;
               |
               | &lt;script type="module" src="https://cdn.jsdelivr.net/npm/material-dynamic-colors@0.1.7/dist/cdn/material-dynamic-colors.min.js"&gt;&lt;/script&gt;
             .medium-space
--- src/home/tooltips.vue
+++ src/home/tooltips.vue
@@ -6,7 +6,24 @@
     a.chip.circle(@click="domain.showSamples(data, '#tooltips nav a', 'Tooltips', null, 'https://github.com/beercss/beercss/blob/main/docs/TOOLTIP.md')")
       i code
   nav.wrap
-    h5 common tooltip
+    label.radio
+      input(
+        type="radio",
+        name="space-toasts",
+        checked,
+        @click="domain.updateSpace('#tooltips .tooltip')"
+      )
+      span no-space
+    label.radio
+      input(type="radio", name="space-toasts", @click="domain.updateSpace('#tooltips .tooltip', 'small-space')")
+      span small-space
+    label.radio
+      input(type="radio", name="space-toasts", @click="domain.updateSpace('#tooltips .tooltip', 'medium-space')")
+      span medium-space
+    label.radio
+      input(type="radio", name="space-toasts", @click="domain.updateSpace('#tooltips .tooltip', 'large-space')")
+      span large-space
+  nav.wrap
     a.chip.circle
       i arrow_back
       .tooltip.left Some text here
@@ -19,32 +36,6 @@
     a.chip.circle
       i arrow_forward
       .tooltip.right Some text here
-    h5 tooltip with gap
-    a.chip.circle
-      i arrow_back
-      .tooltip.tiny-gap Some text here
-    a.chip.circle
-      i arrow_upward
-      .tooltip.small-gap Some text here
-    a.chip.circle
-      i arrow_downward
-      .tooltip.gap Some text here
-    a.chip.circle
-      i arrow_forward
-      .tooltip.large-gap Some text here
-    h5 fade effect
-    a.chip.circle
-      i arrow_back
-      .tooltip.left.fade Some text here
-    a.chip.circle
-      i arrow_upward
-      .tooltip.fade Some text here
-    a.chip.circle
-      i arrow_downward
-      .tooltip.bottom.fade Some text here
-    a.chip.circle
-      i arrow_forward
-      .tooltip.right.fade Some text here
 </template>
 
 <script setup lang="ts">
