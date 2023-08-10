a4ec32a6e1e50c4a7003ddc77351a10e449ac399
Leonardo Rafael Wehrmeister <>
2023-08-10 12:01:13 -0300

    Adding default icon size to chips

--- src/home/chips.vue
+++ src/home/chips.vue
@@ -7,50 +7,50 @@
       i code
   selectionForButtons(context="#chips nav .chip", :colors="[]" :sizes="['', 'medium', 'large']", :directions="['horizontal', 'vertical']", selectedDirection="horizontal", emptySize="small")
   nav.wrap
-    a.chip
-      i.small today
-      span Assist
-    a.chip
-      i.small done
-      span Filter
-    a.chip
-      span Input
-      i.small close
     a.chip Suggestion
     a.chip
+      span Input
+      i close
+    a.chip
+      i done
+      span Filter
+    a.chip
+      i.primary-text today
+      span Assist
+    a.chip
       img(:src="'favicon.png'")
       span Image
     a.chip
       img.responsive(:src="'favicon.png'")
       span Responsive
   nav.wrap
-    a.chip.round
-      i.small today
-      span Assist
-    a.chip.round
-      i.small done
-      span Filter
-    a.chip.round
-      span Input
-      i.small close
     a.chip.round Suggestion
     a.chip.round
+      span Input
+      i close
+    a.chip.round
+      i done
+      span Filter
+    a.chip.round
+      i.primary-text today
+      span Assist
+    a.chip.round
       img(:src="'favicon.png'")
       span Image
     a.chip.round
       img.responsive(:src="'favicon.png'")
       span Responsive
   nav.wrap
-    a.chip.fill
-      i.small today
-      span Assist
-    a.chip.fill
-      i.small done
-      span Filter
+    a.chip.fill Suggestion
     a.chip.fill
       span Input
-      i.small close
-    a.chip.fill Suggestion
+      i close
+    a.chip.fill
+      i done
+      span Filter
+    a.chip.fill
+      i.primary-text today
+      span Assist
     a.chip.fill
       img(:src="'favicon.png'")
       span Image
@@ -58,16 +58,16 @@
       img.responsive(:src="'favicon.png'")
       span Responsive
   nav.wrap
-    a.chip.fill.round
-      i.small today
-      span Assist
-    a.chip.fill.round
-      i.small done
-      span Filter
+    a.chip.fill.round Suggestion
     a.chip.fill.round
       span Input
-      i.small close
-    a.chip.fill.round Suggestion
+      i close
+    a.chip.fill.round
+      i done
+      span Filter
+    a.chip.fill.round
+      i.primary-text today
+      span Assist
     a.chip.fill.round
       img(:src="'favicon.png'")
       span Image
