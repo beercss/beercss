19101485368138f37d841a675c9429ace7342f4f
Leonardo Rafael Wehrmeister <>
2023-08-14 17:25:44 -0300

    Trying to be like MD2 stepper, compatible with theme and disable buttons of MD3

--- src/home/steppers.vue
+++ src/home/steppers.vue
@@ -9,59 +9,53 @@
       i code
   nav.wrap
     label.radio
-      input(type="radio", name="color-steppers", checked, @click="domain.updateElementColor('#steppers button')")
+      input(type="radio", name="color-steppers", checked, @click="domain.updateColor('#steppers button')")
       span default
     label.radio
-      input(type="radio", name="color-steppers", @click="domain.updateElementColor('#steppers button', 'fill')")
+      input(type="radio", name="color-steppers", @click="domain.updateColor('#steppers button', 'fill')")
       span fill
     label.radio
-      input(type="radio", name="color-steppers", @click="domain.updateElementColor('#steppers button', '')")
+      input(type="radio", name="color-steppers", @click="domain.updateColor('#steppers button', '')")
       span primary
     label.radio
-      input(type="radio", name="color-steppers", @click="domain.updateElementColor('#steppers button', 'secondary')")
+      input(type="radio", name="color-steppers", @click="domain.updateColor('#steppers button', 'secondary')")
       span secondary
     label.radio
-      input(type="radio", name="color-steppers", @click="domain.updateElementColor('#steppers button', 'tertiary')")
+      input(type="radio", name="color-steppers", @click="domain.updateColor('#steppers button', 'tertiary')")
       span tertiary
-    label.radio
-      input(type="radio", name="size-steppers", @click="domain.updateSize('#steppers button', 'small')")
-      span small
-    label.radio
-      input(type="radio", name="size-steppers",  checked, @click="domain.updateSize('#steppers button', '')")
-      span medium
-    label.radio
-      input(type="radio", name="size-steppers", @click="domain.updateSize('#steppers button', 'large')")
-      span large
   .medium-space
   nav.scroll
-    button.circle 1
+    button.circle.small
+      i done
     .max.divider
-    button.circle 2
+    button.circle.small 2
     .max.divider
-    button.circle 3
+    button.circle.small(disabled) 3
   .medium-space
   nav.scroll
-    button.circle 1
-    div First
+    button.circle.small
+      i done
+    div Previous
     .max.divider
-    button.circle 2
-    div Second
+    button.circle.small 2
+    div Current
     .max.divider
-    button.circle 3
-    div Third
+    button.circle.small(disabled) 3
+    div Next
   .medium-space
   nav.scroll
     .center-align
-      button.circle 1
-      .small-margin First
+      button.circle.small
+        i done
+      .small-margin Previous
     .max.divider
     .center-align
-      button.circle 2
-      .small-margin Second
+      button.circle.small 2
+      .small-margin Current
     .max.divider
     .center-align
-      button.circle 3
-      .small-margin Third
+      button.circle.small(disabled) 3
+      .small-margin Next
 </template>
 
 <script setup lang="ts">
