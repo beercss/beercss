c16fb39592be3a00ba8c0f9900680b800ab1d758
Leonardo Rafael Wehrmeister <>
2023-09-03 14:32:50 -0300

    Revert "Fixing input label and textarea padding"
    This reverts commit c6f4dbd608f506179a432ccd413f8435d10af67b.

--- src/home/page.vue
+++ src/home/page.vue
@@ -256,9 +256,9 @@ div
             .medium-space
             h5 From CDN
             pre.scroll.large-padding.fill(style="font-family: courier new")
-              | &lt;link href="https://cdn.jsdelivr.net/npm/beercss@3.3.4/dist/cdn/beer.min.css" rel="stylesheet"&gt;
+              | &lt;link href="https://cdn.jsdelivr.net/npm/beercss@3.3.3/dist/cdn/beer.min.css" rel="stylesheet"&gt;
               |
-              | &lt;script type="module" src="https://cdn.jsdelivr.net/npm/beercss@3.3.4/dist/cdn/beer.min.js"&gt;&lt;/script&gt;
+              | &lt;script type="module" src="https://cdn.jsdelivr.net/npm/beercss@3.3.3/dist/cdn/beer.min.js"&gt;&lt;/script&gt;
               |
               | &lt;script type="module" src="https://cdn.jsdelivr.net/npm/material-dynamic-colors@1.0.1/dist/cdn/material-dynamic-colors.min.js"&gt;&lt;/script&gt;
             .medium-space
