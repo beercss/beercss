b40a0dee999bc1694ee03825f951d9a152cd72a4
Leonardo Rafael Wehrmeister <>
2024-06-18 08:03:43 -0300

    Adding multi level menu

--- src/home/menus.vue
+++ src/home/menus.vue
@@ -74,6 +74,22 @@
         a Item 1
         a Item 2
         a Item 3
+  nav.wrap
+    button
+      span Multi level
+      i arrow_drop_down
+      menu
+        a Item
+        a Item
+        a More items
+        menu
+          a Item
+          a Item
+          a More items
+          menu
+            a Item
+            a Item
+            a item
 </template>
 
 <script setup lang="ts">
