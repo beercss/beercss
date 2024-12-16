d2d6c39bf7904da954837897a5dc3a220d21814f
Leonardo Rafael Wehrmeister <>
2024-12-16 18:12:52 -0300

    Updating waves and ripples section

--- src/home/wavesAndRipples.vue
+++ src/home/wavesAndRipples.vue
@@ -3,18 +3,19 @@
   .large-space
   h4.middle-align 
     span Waves and ripples
-    button.chip.circle(@click="domain.showSamples(data, '#waves-and-ripples-example button, #waves-and-ripples-example article, #waves-and-ripples-example a', 'Waves and ripples', null, 'https://github.com/beercss/beercss/blob/main/docs/TYPOGRAPHY.md')")
+    button.chip.circle(@click="domain.showSamples(data, '#waves-and-ripples-example button, #waves-and-ripples-example article, #waves-and-ripples-example a', 'Waves and ripples', null, 'https://github.com/beercss/beercss/blob/main/docs/HELPERS.md')")
       i code
   .medium-space
   #waves-and-ripples-example
-    button.wave Wave
-    button.ripple Ripple
-    button.slow-ripple Slow ripple
-    button.fast-ripple Fast ripple
-    button.chip.wave Wave
-    button.chip.ripple Ripple
-    button.chip.slow-ripple Slow ripple
-    button.chip.fast-ripple Fast ripple
+    nav.wrap
+      button.wave Wave
+      button.ripple Ripple
+      button.slow-ripple Slow ripple
+      button.fast-ripple Fast ripple
+      button.chip.wave Wave
+      button.chip.ripple Ripple
+      button.chip.slow-ripple Slow ripple
+      button.chip.fast-ripple Fast ripple
     nav.wrap
       article.small-width.small-height.wave
         h6 Wave
