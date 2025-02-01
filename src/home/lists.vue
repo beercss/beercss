d83510889a17ad66742a6b7a27d395dfc61b1287
Leonardo Rafael Wehrmeister <>
2025-01-31 21:14:35 -0300

    Adding list using ul, ol, li

--- src/home/lists.vue
+++ src/home/lists.vue
@@ -4,101 +4,130 @@ div
     .large-space
     h4
       span Lists
-      button.chip.circle(@click="domain.showSamples(data, '#list .row', 'Lists')")
-        i code
-    .medium-space
-    #list
-      h6 Headline and supporting text
-      .space
-      a.row.padding.surface-container
-        button.circle A
-        .max
-          h6.small Headline
-        label +15 min
-      .space
-      a.row.padding.surface-container
-        button.circle A
-        .max
-          h6.small Headline
-          div Supporting text
-        label +15 min
-      .medium-space
-      h6 Icons
-      .space
-      a.row.padding.surface-container
-        i home
-        .max
-          h6.small Headline
-        label +15 min
-      .space
-      a.row.padding.surface-container
-        i home
-        .max
-          h6.small Headline
-          div Supporting text
-        label +15 min
-      .medium-space
-      h6 Leading media or avatar
-      .space
-      a.row.padding.surface-container
-        img.round(:src="'/beer-and-woman.svg'")
-        .max
-          h6.small Headline
-        label +15 min
-      .space
-      a.row.padding.surface-container
-        img.round(:src="'/beer-and-woman.svg'")
-        .max
-          h6.small Headline
-          div Supporting text
-        label +15 min
-      .medium-space
-      h6 Buttons and links
-      .space
-      nav.row.padding.surface-container
-        .max
-          h6.small Headline
-        a Link
-        button Button
-      .space
-      nav.row.padding.surface-container
-        .max
-          h6.small Headline
-          div Supporting text
-        a Link
-        button Button
     .medium-space
     .grid
-      #list-dividers.s12.m6.l6
+      #list-default.s12.m6.l6
         h6
-          span List dividers
-          button.chip.circle(@click="domain.showSamples(data, '#list-dividers article', 'List containers')")
+          span Default
+          button.chip.circle(@click="domain.showSamples(data, '#list-default > article > .list', 'List border')")
             i code
         article
-          a.row.wave Item 1
-          hr
-          a.row.wave Item 2
-          hr
-          a.row.wave Item 3
-          hr
-          a.row.wave Item 4
-          hr
-          a.row.wave Item 5
-      #list-expansions.s12.m6.l6
+          ul.list.border
+            li Item
+            li Item
+            li Item
+            li Item
+      #list-nested.s12.m6.l6
         h6
-          span List expansions
-          button.chip.circle(@click="domain.showSamples(data, '#list-expansions article', 'List containers')")
+          span Nested
+          button.chip.circle(@click="domain.showSamples(data, '#list-nested > article > .list', 'List nested')")
             i code
         article
-          details
-            summary.none 
-              a.row.wave Header Item
-            a.row.wave Item
-            a.row.wave Item
-            details
-              summary.none 
-                a.row.wave Header Item
-              a.row.wave Item
+          ul.list.border
+            li Item
+            li Item
+            li
+              ul.list.border
+                li Item
+                li Item
+      #list-expansion.s12.m6.l6
+        h6
+          span Expansion
+          button.chip.circle(@click="domain.showSamples(data, '#list-expansion > article > .list', 'List expansion')")
+            i code
+        article
+          ul.list.border
+            li Item
+            li Item
+            li
+              details
+                summary Header Item
+                ul.list.border
+                  li Item
+                  li Item
+                  li
+                    details
+                      summary Header Item
+                      ul.list.border
+                        li Item
+                        li Item
+    .medium-space
+    #list1
+      h6 
+        span Headline and supporting text
+        button.chip.circle(@click="domain.showSamples(data, '#list1 .list', 'Lists')")
+          i code
+      .space
+      ul.list.border
+        li
+          button.circle A
+          .max
+            h6.small Headline
+          label +15 min
+        li
+          button.circle A
+          .max
+            h6.small Headline
+            div Supporting text
+          label +15 min
+    .medium-space
+    #list2
+      h6 
+        span Icons
+        button.chip.circle(@click="domain.showSamples(data, '#list2 .list', 'Lists')")
+          i code
+      .space
+      ul.list.border
+        li
+          i home
+          .max
+            h6.small Headline
+          label +15 min
+        li
+          i home
+          .max
+            h6.small Headline
+            div Supporting text
+          label +15 min
+    .medium-space
+    #list3
+      h6 
+        span Leading media or avatar
+        button.chip.circle(@click="domain.showSamples(data, '#list3 .list', 'Lists')")
+          i code
+      .space
+      ul.list.border
+        li
+          img.round(:src="'/beer-and-woman.svg'")
+          .max
+            h6.small Headline
+          label +15 min
+        li
+          img.round(:src="'/beer-and-woman.svg'")
+          .max
+            h6.small Headline
+            div Supporting text
+          label +15 min
+    .medium-space
+    #list4
+      h6 
+        span Links and buttons
+        button.chip.circle(@click="domain.showSamples(data, '#list4 .list', 'Lists')")
+          i code
+      .space
+      ul.list.border
+        li
+          .max
+            h6.small Headline
+          a Link
+          button Button
+        li
+          .max
+            h6.small Headline
+            div Supporting text
+          a Link
+          button Button
+    
 </template>
 
 <script setup lang="ts">
