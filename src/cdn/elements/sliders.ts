340e224f55e1bb7d6d2b87507031c8270c440a47
Leonardo Rafael Wehrmeister <>
2025-02-11 09:40:15 -0300

    Fixing size root, to work with default browser size

--- src/cdn/elements/sliders.ts
+++ src/cdn/elements/sliders.ts
@@ -36,6 +36,13 @@ function updateAllRanges() {
   for(let i=0; i<ranges.length; i++) updateRange(ranges[i]);
 }
 
+function rootSizeInPixels(): number {
+  const size = getComputedStyle(document.documentElement).getPropertyValue("--size") || "16px";
+  if (size.indexOf("%") != -1) return (parseInt(size) * 16) / 100;
+  if (size.indexOf("em") != -1) return parseInt(size) * 16;
+  return parseInt(size);
+} 
+
 function updateRange(input: HTMLInputElement) {
   on(input, "focus", onFocusRange);
   on(input, "blur", onBlurRange);
@@ -45,7 +52,7 @@ function updateRange(input: HTMLInputElement) {
   const inputs = queryAll("input", label) as NodeListOf<HTMLInputElement>;
   if (!inputs.length || !bar) return;
 
-  const rootSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--size")) || 16;
+  const rootSize = rootSizeInPixels();
   const thumb = hasClass(label, "max") ? 0 : 0.25 * rootSize * 100 / inputs[0].offsetWidth;
   const percents: Array<number> = [];
   const values: Array<number> = [];
