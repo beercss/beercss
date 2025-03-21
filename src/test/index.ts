4d3e60c626f13e3c04be64304c865d633a7ea5e9
Leonardo Rafael Wehrmeister <>
2025-03-21 15:37:23 -0300

    Updating main layouts

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,4 +1,6 @@
 import { router } from "../shared/router";
 import test from "./page.vue";
+import mainLayouts from "./mainLayouts.vue";
 
+router("/test/mainLayouts", mainLayouts);
 router("/test", test);
---
+++ src/test/mainLayouts.vue
@@ -0,0 +1,76 @@
+<template lang="pug">
+div(:dir="rtlValue ? 'rtl' : ''", :style="safeAreaValue ? '--top:24px;--bottom:24px;--left:24px;--right:24px;' : ''")
+  nav.top(v-if="topValue")
+    div &lt;nav&gt;
+  nav.bottom(v-if="bottomValue")
+    div &lt;nav&gt;
+  nav.left(v-if="leftValue")
+    div &lt;nav&gt;
+  nav.right(v-if="rightValue")
+    div &lt;nav&gt;
+  header(v-if="headerValue", :class="{ fixed: fixedValue, responsive: responsiveValue }")
+    div &lt;header&gt;
+  main(:class="{ responsive: responsiveValue }")
+    aside.right
+      div &lt;aside&gt;
+    div
+      div &lt;main&gt;
+      nav
+        label.checkbox
+          input(type="checkbox", v-model="safeAreaValue")
+          span 24px
+        label.checkbox
+          input(type="checkbox", v-model="rtlValue")
+          span rtl
+        label.checkbox
+          input(type="checkbox", v-model="responsiveValue")
+          span responsive
+        label.checkbox
+          input(type="checkbox", v-model="fixedValue")
+          span fixed
+        label.checkbox
+          input(type="checkbox", v-model="headerValue")
+          span header
+        label.checkbox
+          input(type="checkbox", v-model="footerValue")
+          span footer
+        label.checkbox
+          input(type="checkbox", v-model="topValue")
+          span nav.top
+        label.checkbox
+          input(type="checkbox", v-model="bottomValue")
+          span nav.bottom
+        label.checkbox
+          input(type="checkbox", v-model="leftValue")
+          span nav.left
+        label.checkbox
+          input(type="checkbox", v-model="rightValue")
+          span nav.right
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  footer(v-if="footerValue", :class="{ fixed: fixedValue, responsive: responsiveValue }")
+    div &lt;footer&gt;
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const safeAreaValue = ref(false);
+const rtlValue = ref(false);
+const responsiveValue = ref(false);
+const headerValue = ref(true);
+const footerValue = ref(true);
+const topValue = ref(true);
+const leftValue = ref(true);
+const bottomValue = ref(true);
+const rightValue = ref(true);
+const fixedValue = ref(true);
+</script>
