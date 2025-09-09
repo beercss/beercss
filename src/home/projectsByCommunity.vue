b020e58fa991ee123961ffef2d7e323930d1ca54
Leonardo Rafael Wehrmeister <>
2025-09-09 08:43:34 -0300

    Adding kdomskia

--- src/home/projectsByCommunity.vue
+++ src/home/projectsByCommunity.vue
@@ -6,47 +6,61 @@ section
   .large-space
   .grid
     .s12.m6.l3
+      a.wrap(href="https://github.com/Devolutions/IronRDP")
+        article.round.wave.no-elevate
+          .no-round
+            .center-align
+              img.small-width.small-height(src="https://avatars.githubusercontent.com/u/6097418", loading="lazy")
+            h6.truncate Iron RDP
+    .s12.m6.l3
       a.wrap(href="https://github.com/Materialious/Materialious")
         article.round.wave.no-elevate
-          .center-align
-            img.small-width.small-height(src="https://github.com/Materialious/Materialious/blob/main/materialious/resources/icon.png?raw=true", loading="lazy")
-          h6.truncate Materialious
+          .no-round
+            .center-align
+              img.small-width.small-height(src="https://github.com/Materialious/Materialious/blob/main/materialious/resources/icon.png?raw=true", loading="lazy")
+            h6.truncate Materialious
     .s12.m6.l3
       a.wrap(href="https://github.com/jonasgeiler/svelte-tiny-virtual-list")
         article.round.wave.no-elevate
-          .center-align
-            img.small-width.small-height(src="https://github.com/jonasgeiler/svelte-tiny-virtual-list/raw/main/static/logo.svg", loading="lazy")
-          h6.truncate Svelte tiny virtual list
-    .s12.m6.l3
-      a.wrap(href="https://github.com/Devolutions/IronRDP")
-        article.round.wave.no-elevate
-          .center-align
-            img.small-width.small-height(src="https://avatars.githubusercontent.com/u/6097418", loading="lazy")
-          h6.truncate Iron RDP
-    .s12.m6.l3
-      a.wrap(href="https://github.com/WardPearce/Purplix.io")
-        article.round.wave.no-elevate
-          .center-align
-            img.small-height(src="https://raw.githubusercontent.com/WardPearce/Purplix.io/24682be424e95e61f7355d17d5a9e501a2e6a73d/frontend/public/logo.png", loading="lazy")
-          h6.truncate Purplix.io
+          .no-round
+            .center-align
+              img.small-width.small-height(src="https://github.com/jonasgeiler/svelte-tiny-virtual-list/raw/main/static/logo.svg", loading="lazy")
+            h6.truncate Svelte tiny virtual list
     .s12.m6.l3
       a.wrap(href="https://github.com/m4heshd/ufc-ripper")
         article.round.wave.no-elevate.black
-          .center-align
-            img.small-width.small-height(src="https://github.com/m4heshd/ufc-ripper/raw/master/project-res/images/ufcr-banner.svg", loading="lazy")
-          h6.truncate.white-text UFC Ripper
+          .no-round
+            .center-align
+              img.small-width.small-height(src="https://github.com/m4heshd/ufc-ripper/raw/master/project-res/images/ufcr-banner.svg", loading="lazy")
+            h6.truncate.white-text UFC Ripper
+    .s12.m6.l3
+      a.wrap(href="https://github.com/WardPearce/Purplix.io")
+        article.round.wave.no-elevate
+          .no-round
+            .center-align
+              img.small-height(src="https://raw.githubusercontent.com/WardPearce/Purplix.io/24682be424e95e61f7355d17d5a9e501a2e6a73d/frontend/public/logo.png", loading="lazy")
+            h6.truncate Purplix.io
     .s12.m6.l3
       a.wrap(href="https://github.com/bethropolis/haze-editor")
         article.round.wave.no-elevate
-          .center-align
-            img.small-width.small-height(src="https://github.com/bethropolis/haze-editor/blob/main/public/img.png?raw=true", loading="lazy")
-          h6.truncate Haze editor
+          .no-round
+            .center-align
+              img.small-width.small-height(src="https://github.com/bethropolis/haze-editor/blob/main/public/img.png?raw=true", loading="lazy")
+            h6.truncate Haze editor
+    .s12.m6.l3
+      a.wrap(href="https://github.com/kdomskia/kdomskia")
+        article.round.wave.no-elevate
+          .no-round
+            .center-align
+              img.small-width.small-height(src="https://avatars.githubusercontent.com/u/225685469", loading="lazy")
+            h6.truncate Kdomskia
     .s12.m6.l3
       a.wrap(href="https://github.com/imhlq/EldenRingCheatSheetCN")
         article.round.wave.no-elevate
-          .center-align
-            img.small-width.small-height(src="https://avatars.githubusercontent.com/u/10741526", loading="lazy")
-          h6.truncate Elden Ring Cheat Sheet CN
+          .no-round
+            .center-align
+              img.small-width.small-height(src="https://avatars.githubusercontent.com/u/10741526", loading="lazy")
+            h6.truncate Elden Ring Cheat Sheet CN
 </template>
 
 <script setup lang="ts">
