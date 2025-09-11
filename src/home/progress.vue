9aa6831ab130cd04c76c5be626c4c71ef1e05dc5
Leonardo Rafael Wehrmeister <>
2025-09-11 14:19:52 -0300

    Adding wavy linear progress

--- src/home/progress.vue
+++ src/home/progress.vue
@@ -1,23 +1,8 @@
 <template lang="pug">
 #progress
   .large-space
-  h4
-    span Progress
-    button.chip.circle(@click="domain.showSamples(data, '#default-progress progress, #custom-progress article, #custom-progress nav > button, #custom-progress nav > .chip', 'Progress', null, 'https://github.com/beercss/beercss/blob/main/docs/PROGRESS.md')")
-      i code
+  h4 Progress
   nav.wrap
-    label.radio
-      input(type="radio", name="percent-progress", @click="domain.updateProgress(0)")
-      span 0%
-    label.radio
-      input(type="radio", name="percent-progress", checked, @click="domain.updateProgress(30)")
-      span 30%
-    label.radio
-      input(type="radio", name="percent-progress", @click="domain.updateProgress(60)")
-      span 60%
-    label.radio
-      input(type="radio", name="percent-progress", @click="domain.updateProgress(100)")
-      span 100%
     label.radio
       input(type="radio", name="color-progress", checked, @click="domain.updateTextColor('#progress progress')")
       span default
@@ -28,21 +13,58 @@
       input(type="radio", name="color-progress", @click="domain.updateTextColor('#progress progress', 'orange-text')")
       span orange
   .large-space
-  #default-progress
-    h6 Linear (default)
-    .space
-    progress
-    .space
-    progress(value="30", max="100")
-    .medium-space
-    h6 Circular
+  #linear-progress
+    h6 
+      span Linear
+      button.chip.circle(@click="domain.showSamples(data, '#linear-progress progress', 'Progress', null, 'https://github.com/beercss/beercss/blob/main/docs/PROGRESS.md')")
+        i code
+    nav.wrap
+      label.radio
+        input(type="radio", name="percent-progress", @click="domain.updateProgress(0)")
+        span 0%
+      label.radio
+        input(type="radio", name="percent-progress", checked, @click="domain.updateProgress(30)")
+        span 30%
+      label.radio
+        input(type="radio", name="percent-progress", @click="domain.updateProgress(60)")
+        span 60%
+      label.radio
+        input(type="radio", name="percent-progress", @click="domain.updateProgress(100)")
+        span 100%
+      label.radio
+        input(type="radio", name="size-progress", checked, @click="domain.updateSize('#linear-progress progress')")
+        span small
+      label.radio
+        input(type="radio", name="size-progress", @click="domain.updateSize('#linear-progress progress', 'medium')")
+        span medium
+      label.radio
+        input(type="radio", name="size-progress", @click="domain.updateSize('#linear-progress progress', 'large')")
+        span large
+    .grid
+      .s12.m6.l6
+        progress
+        .space
+        progress(value="30", max="100")
+      .s12.m6.l6
+        progress.wavy
+        .space
+        progress.wavy(value="30", max="100")
+  .large-space
+  #circular-progress 
+    h6 
+      span Circular
+      button.chip.circle(@click="domain.showSamples(data, '#circular-progress progress', 'Progress', null, 'https://github.com/beercss/beercss/blob/main/docs/PROGRESS.md')")
+        i code
     nav
       progress.circle.small
       progress.circle
       progress.circle.large
   .large-space
   #custom-progress
-    h6 Custom progressbar
+    h6 
+      span Custom progressbar
+      button.chip.circle(@click="domain.showSamples(data, '#custom-progress progress', 'Progress', null, 'https://github.com/beercss/beercss/blob/main/docs/PROGRESS.md')")
+        i code
     .medium-space
     nav
       article
