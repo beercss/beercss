843e7f59a1ce1358283f2f4e7856d736fd1b1af1
Leonardo Rafael Wehrmeister <leo.bnu@gmail.com>
2026-01-28 08:02:57 -0300

    chore: updated readme to brotli size

--- build/readme.js
+++ build/readme.js
@@ -1,10 +1,10 @@
-import { gzipSizeSync } from 'gzip-size';
+import brotliSize from 'brotli-size';
 import fs from "fs";
 
 export default async function() {
   const css = fs.readFileSync("./dist/cdn/beer.min.css", "utf-8");
   const readme = fs.readFileSync("./README.md", "utf-8");
-  const size = (gzipSizeSync(css)/1024).toFixed(1);
+  const size = (brotliSize.sync(css)/1024).toFixed(1);
 
-  fs.writeFileSync("./README.md", readme.replace(/gzip_size-[0-9.]+kb-green/, `gzip_size-${size}kb-green`));
+  fs.writeFileSync("./README.md", readme.replace(/brotli_size-[0-9.]+kb-green/, `brotli_size-${size}kb-green`));
 }
