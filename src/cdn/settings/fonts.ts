6e4f8af171e595dad4ebe4ee8cf5b2fbbe044af5
Leonardo Rafael Wehrmeister <leo.bnu@gmail.com>
2026-05-15 11:07:28 -0300

    fea: using js object instead array, to optimize unique icons

--- src/cdn/settings/fonts.ts
+++ src/cdn/settings/fonts.ts
@@ -1,38 +1,42 @@
 import { queryAll } from "../utils";
 
-let _icons: string[] = [];
-let _fontIcon = "";
+let _icons:any = null;
+let _font = "";
 
 function updateIcons() {
-  _fontIcon = _fontIcon || getComputedStyle(document.documentElement).getPropertyValue('--font-icon').replace(/\"/g, "");
-  if (_fontIcon.indexOf(" Auto") == -1) return false;
+  _font = _font || getComputedStyle(document.documentElement).getPropertyValue('--font-icon').replace(/\"/g, "");
+  if (_font.indexOf(" Auto") == -1) return false;
 
   let hasNewIcons = false;
-  if (!_icons.length) {
-    _icons = ["check", "check_box", "check_box_outline_blank", "indeterminate_check_box", "radio_button_checked", "radio_button_unchecked"];
+  if (!_icons) {
+    _icons = { "check": true, "check_box": true, "check_box_outline_blank": true, "indeterminate_check_box": true, "radio_button_checked": true, "radio_button_unchecked": true };
     hasNewIcons = true;
   }
 
   const icons = queryAll("i");
   for(let i=0; i<icons.length; i++) {
-    const iconName = icons[i].textContent?.trim();
-    if (!iconName || _icons.indexOf(iconName) != -1) continue;
-
-    hasNewIcons = true;
-    _icons.push(iconName);
+    const icon = icons[i].textContent?.trim();
+    if (!icon) continue;
+    if (!_icons[icon]) {
+      _icons[icon] = true;
+      hasNewIcons = true;
+    }
   }
   
   return hasNewIcons;
 }
 
 async function updateFont() {
-  const name = _fontIcon.replace(" Auto", "");
-  const response = await fetch(`https://fonts.googleapis.com/css2?family=${name}:opsz,wght,FILL,GRAD@24,400,0..1,0&icon_names=${_icons.sort().join(',')}`);
-  const text = await response.text() || "";
-  const matches = text.match(/url.+?\)/);
-  if (!matches) return;
+  if (!_icons) return;
 
-  document.fonts.add(new FontFace(_fontIcon, matches[0], {
+  const icons = Object.keys(_icons).sort().join(",");
+  const font = _font.replace(" Auto", "");
+  const response = await fetch(`https://fonts.googleapis.com/css2?family=${font}:opsz,wght,FILL,GRAD@24,400,0..1,0&icon_names=${icons}`);
+  const text = await response.text() || "";
+  const urls = text.match(/url.+?\)/);
+  if (!urls) return;
+
+  document.fonts.add(new FontFace(_font, urls[0], {
     weight: "400",
     style: "normal",
     display: "swap"
